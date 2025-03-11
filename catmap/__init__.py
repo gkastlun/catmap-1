fbfa02c71079845ed9e76b693741fcdef7cbdcd7
Georg Kastlunger <geokast@lappi2.fys.clients.local>
2025-03-11 12:43:34 +0100

    Took out print

--- catmap/__init__.py
+++ catmap/__init__.py
@@ -34,9 +34,9 @@ from scipy.interpolate import griddata as sp_griddata
 
 def griddata(*args, **kwargs):
     """Wrapper function to avoid annoying griddata errors"""
-    print(*args)
-    print(**kwargs)
-    ddd
+    #print(*args)
+    #print(**kwargs)
+    #ddd
     try:
         return sp_griddata(*args, **kwargs)
     except RuntimeError:
